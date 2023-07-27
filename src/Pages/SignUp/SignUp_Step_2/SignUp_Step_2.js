c2282010099df80a90e76f8525dd813a41f414ed
thisisamal <amalpullan4@gmail.com>
2023-07-27 08:33:15 +0530

    done

--- src/Pages/SignUp/SignUp_Step_2/SignUp_Step_2.js
+++ src/Pages/SignUp/SignUp_Step_2/SignUp_Step_2.js
@@ -36,7 +36,7 @@ export function SignUp_Step_2() {
       const sendData = () => {
         const requestData = { "firstname":firstname,"lastname":lastname};
         let url = core_url+'/signup__'
-        axios.post(core_url, requestData)
+        axios.post(url, requestData)
         .then(response => {
         setdata(response.data);
         })
